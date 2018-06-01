e83298b35cb95f50eaeb96e9b87beb37a433d738
liuliangsir <2269305724@qq.com>
2018-06-01 10:35:00 +0800

    add new breaking change feature

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const babelCore = require('babel-core');
+const babelCore = require('@babel/core');
 const helper = require('think-helper');
 const fs = require('fs');
 const path = require('path');
@@ -9,16 +9,16 @@ const path = require('path');
  * @return {Boolean}
  */
 function babelTranspile(config) {
-  const {srcPath, outPath, file, ext = '.js'} = config;
-  let {options} = config;
-  const filePath = path.join(srcPath, file);
-  const pPath = path.dirname(path.join(outPath, file));
-  const content = fs.readFileSync(filePath, 'utf8');
-  const relativePath = path.relative(pPath, path.join(srcPath, file));
+  const {srcPath, outPath, file: filename, ext = '.js'} = config;
+
+  const srcAbsolutePath = path.join(srcPath, filename);
+  const outputAbsolutePath = path.join(outPath, filename);
+  const relativePath = path.relative(path.dirname(outputAbsolutePath), srcAbsolutePath);
 
   // babel options
+  let {options} = config;
   options = Object.assign({
-    filename: file,
+    filename,
     sourceFileName: relativePath,
     sourceMaps: true,
     babelrc: false
@@ -27,17 +27,19 @@ function babelTranspile(config) {
   // babel transform
   let data;
   try {
+    const content = fs.readFileSync(srcAbsolutePath, 'utf8');
     data = babelCore.transform(content, options);
   } catch (e) {
     return e;
   }
 
   // write es5 code file
-  const outFile = path.join(outPath, file).replace(/\.\w+$/, ext);
+  const outFile = outputAbsolutePath.replace(/\.\w+$/, ext);
   helper.mkdir(path.dirname(outFile));
-  const basename = path.basename(file).replace(/\.\w+$/, ext);
+
+  const basename = path.basename(filename).replace(/\.\w+$/, ext);
   const prefix = '//# sourceMappingURL=';
-  if (data.code.indexOf(prefix) === -1 && options.sourceMaps) {
+  if (data.code.indexOf(prefix) < 0 && options.sourceMaps) {
     data.code = data.code + '\n' + prefix + basename + '.map';
   }
   fs.writeFileSync(outFile, data.code);
